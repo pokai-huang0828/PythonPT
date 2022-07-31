c4509ba6f6afdd681c2e22516223b0a59af0afa7
pokai-huang0828 <000429977@student.vcc.ca>
2022-07-31 10:17:24 +0800

    Update new import package practice

--- Example/WordCount.py
+++ Example/WordCount.py
@@ -1,35 +1,35 @@
 song = """Are you sleeping, are you sleeping, Brother John, Brother John?
 Morning bells are ringing, morning bells are ringing.
 Ding ding dong, Ding ding dong."""
-wdict = {}                           # 空字典未來儲存單字計數結果
+wdict = {}  # 空字典未來儲存單字計數結果
 print("原始歌詞")
 print(song)
 
 # 將歌曲大寫字母改成小寫
-songLower = song.lower()            # 歌曲改為小寫
+songLower = song.lower()  # 歌曲改為小寫
 print("小寫歌詞")
 print(songLower)
 
 # 去掉標點符號
-for ch in songLower:                
-    if ch in ".,?":                 # 將歌曲的標點符號用空字元取代
-        songLower = songLower.replace(ch,'')
-print("去掉標點符號")    
+for ch in songLower:
+    if ch in ".,?":  # 將歌曲的標點符號用空字元取代
+        songLower = songLower.replace(ch, '')
+print("去掉標點符號")
 print(songLower)
 
 # 將歌曲字串轉成串列
-songList = songLower.split()        
+songList = songLower.split()
 print("轉換成串列")
-print(songList)                     # 列印歌曲串列
+print(songList)  # 列印歌曲串列
 
 # 將歌曲串列處理成字典 
-for wd in songList:                 
+for wd in songList:
     '''if wd in wdict:              # 檢查此字是否已在字典內
         wdict[wd] += 1           # 累計出現次數
     else:
         wdict[wd] = 1            # 第一次出現的字建立此鍵與值'''
     count = wdict.get(wd, 0)
-    wdict.update({wd:count+1})
-    
+    wdict.update({wd: count + 1})
+
 print("單字及出現次數")
-print(wdict)                         # 列印字典
+print(wdict)  # 列印字典
