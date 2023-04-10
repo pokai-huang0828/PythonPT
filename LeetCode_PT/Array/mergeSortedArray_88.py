b404648539c1288b948996fc78fa769cb21832da
pokai-huang0828 <kenny0963676060@gmail.com>
2023-04-10 16:29:04 +0800

    Updated mergeSortedArray_88.py

--- LeetCode_PT/Array/mergeSortedArray_88.py
+++ LeetCode_PT/Array/mergeSortedArray_88.py
@@ -43,3 +43,63 @@
 '''
 
 import unittest
+
+def merge(nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+    """
+    Do not return anything, modify nums1 in-place instead.
+    """
+    # Initialize a variable to store the index of the last element in nums1
+    last_index = m + n - 1
+    # Initialize a variable to store the index of the last element in nums2
+    nums2_index = n - 1
+    # Initialize a variable to store the index of the last element in nums1
+    nums1_index = m - 1
+    # While the index of the last element in nums1 is greater than or equal to 0
+    # and the index of the last element in nums2 is greater than or equal to 0
+    while nums1_index >= 0 and nums2_index >= 0:
+        # If the value of the last element in nums1 is greater than or equal to the value of the last element in nums2
+        if nums1[nums1_index] >= nums2[nums2_index]:
+            # Set the value of the last element in nums1 to the value of the last element in nums1
+            nums1[last_index] = nums1[nums1_index]
+            # Decrement the index of the last element in nums1
+            nums1_index -= 1
+        else:
+            # Set the value of the last element in nums1 to the value of the last element in nums2
+            nums1[last_index] = nums2[nums2_index]
+            # Decrement the index of the last element in nums2
+            nums2_index -= 1
+        # Decrement the index of the last element in nums1
+        last_index -= 1
+    # While the index of the last element in nums2 is greater than or equal to 0
+    while nums2_index >= 0:
+        # Set the value of the last element in nums1 to the value of the last element in nums2
+        nums1[last_index] = nums2[nums2_index]
+        # Decrement the index of the last element in nums2
+        nums2_index -= 1
+        # Decrement the index of the last element in nums1
+        last_index -= 1
+
+    return nums1
+    
+'''
+    The time complexity of the above algorithm is O(m + n)O(m+n)O(m+n) as we are iterating through both arrays containing ‘m’ and ‘n’ elements respectively.
+    
+    The space complexity will be O(1)O(1)O(1) as we are not using any extra space.
+'''
+
+# Test Cases
+num1 = [1,2,3,0,0,0]
+m = 3
+num2 = [2,5,6]
+n = 3
+
+print(merge(num1, m, num2, n))
+
+class test_merge_function(unittest.TestCase):
+    def test_merge(self):
+        self.assertEqual(merge([1,2,3,0,0,0], 3, [2,5,6], 3), [1,2,2,3,5,6])
+        self.assertEqual(merge([1], 1, [], 0), [1])
+        self.assertEqual(merge([2,3,0,0], 2, [1,5], 2), [1,2,3,5])
+        
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False, verbosity=2)
